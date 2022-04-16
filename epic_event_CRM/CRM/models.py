aaf81d6dba4cfbc2e40d43b86d545e2cac91bb3b
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-04-16 21:38:55 +0200

    update model

--- epic_event_CRM/CRM/models.py
+++ epic_event_CRM/CRM/models.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 
 class SalesStaff(models.Model):
     full_name = models.CharField(max_length=255)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, 
+            on_delete=models.CASCADE,)
     phone = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        if GestionStaff.objects.filter(user=self.user).exists() or SupportStaff.objects.filter(user=self.user).exists():
+            raise ValueError("This user has already a role")
+        super(SalesStaff, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.full_name
@@ -15,6 +21,11 @@ class GestionStaff(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=255)
 
+    def save(self, *args, **kwargs):
+        if SalesStaff.objects.filter(user=self.user).exists() or SupportStaff.objects.filter(user=self.user).exists():
+            raise ValueError("This user has already a role")
+        super(GestionStaff, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.full_name
 
@@ -22,6 +33,11 @@ class SupportStaff(models.Model):
     full_name = models.CharField(max_length=255)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        if SalesStaff.objects.filter(user=self.user).exists() or GestionStaff.objects.filter(user=self.user).exists():
+            raise ValueError("This user has already a role")
+        super(SupportStaff, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.full_name
@@ -33,10 +49,11 @@ class Client(models.Model):
     email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=255)
     mobile = models.CharField(max_length=255)
-    company_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    sales_staff = models.ForeignKey(to=SalesStaff, on_delete=models.CASCADE, null=True)
+    sales_staff = models.ForeignKey(to=SalesStaff, on_delete=models.CASCADE)
+    converted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.first_name
@@ -50,8 +67,8 @@ class Contract(models.Model):
     payment_due = models.DateTimeField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    sales_staff = models.ForeignKey(to=SalesStaff, on_delete=models.CASCADE, null=True)
-    client = models.ForeignKey(to=Client, on_delete=models.CASCADE, null=True)
+    sales_staff = models.ForeignKey(to=SalesStaff, on_delete=models.CASCADE)
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -75,8 +92,9 @@ class Event(models.Model):
     notes = models.TextField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    support_staff = models.ForeignKey(to=SupportStaff, on_delete=models.CASCADE, null=True)
-    event_status = models.ForeignKey(to=EventStatus, on_delete=models.CASCADE, null=True)
+    client = models.ForeignKey(to=Client, on_delete=models.CASCADE, null=True)
+    support_staff = models.ForeignKey(to=SupportStaff, on_delete=models.CASCADE)
+    event_status = models.ForeignKey(to=EventStatus, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
